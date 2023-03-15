e808224424f51b5e1d57b6cfae6a4db151ec0bcb
Luke Collishaw <lc796@exeter.ac.uk>
2023-03-15 19:36:17 +0000

    Add Leaflet script and style

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+                integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+                crossOrigin=""/>
+      </Head>
       <body>
         <Main />
         <NextScript />
