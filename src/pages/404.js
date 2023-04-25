1070758169cf452bd541c60bb340f424d8428438
Luke Collishaw <lc796@exeter.ac.uk>
2023-04-25 17:44:17 +0100

    Update 404 page and style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,22 @@
+import styles from "@/styles/404.module.css";
+import Head from "next/head";
+
 export default function FourOhFour() {
-    return <div>404 - Page Not Found</div>
+    return (
+        <>
+            <Head>
+                <title>Error 404</title>
+                <meta name="description" content="Shows latest disasters"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <h1>Error 404 - Page Not Found!</h1>
+                    <p>The route you are trying to access does not seem to exist.</p>
+                    <p><a href={"/"}>Click to return home.</a></p>
+                </div>
+            </div>
+        </>
+    )
 }
